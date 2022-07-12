fbfdea1f0647f316b7ea9ed046f317a6a1c5b545
Aleksandr Golynin <p14y3r011@yandex.ru>
2022-07-13 03:40:48 +0500

    BACK11 - New fields added, mapping is fixed also

--- src/main/java/kata/academy/socialnetwork/model/converter/UserMapper.java
+++ src/main/java/kata/academy/socialnetwork/model/converter/UserMapper.java
@@ -5,30 +5,48 @@ import kata.academy.socialnetwork.model.dto.request.user.UserUpdatePasswordReque
 import kata.academy.socialnetwork.model.dto.request.user.UserUpdateRequestDto;
 import kata.academy.socialnetwork.model.dto.response.user.UserResponseDto;
 import kata.academy.socialnetwork.model.entity.User;
+import kata.academy.socialnetwork.model.entity.UserInfo;
 
 public final class UserMapper {
 
     public static User toEntity(UserPersistRequestDto dto) {
         User user = new User();
+        UserInfo userInfo = new UserInfo();
         user.setEmail(dto.email());
         user.setPassword(dto.password());
-        user.setFirstName(dto.firstName());
-        user.setLastName(dto.lastName());
+        userInfo.setFirstName(dto.firstName());
+        userInfo.setLastName(dto.lastName());
+        userInfo.setCity(dto.city());
+        userInfo.setAge(dto.age());
+        userInfo.setGender(dto.gender());
+        user.setUserInfo(userInfo);
         return user;
     }
 
+
+
     public static UserResponseDto toDto(User user) {
+
         return new UserResponseDto(
                 user.getId(),
                 user.getEmail(),
-                user.getFirstName(),
-                user.getLastName()
+                user.getUserInfo().getFirstName(),
+                user.getUserInfo().getLastName(),
+                user.getUserInfo().getCity(),
+                user.getUserInfo().getAge(),
+                user.getUserInfo().getGender()
+
         );
     }
 
     public static User updateUser(User user, UserUpdateRequestDto dto) {
-        user.setFirstName(dto.firstName());
-        user.setLastName(dto.lastName());
+        UserInfo userInfo = user.getUserInfo();
+        userInfo.setFirstName(dto.firstName());
+        userInfo.setLastName(dto.lastName());
+        userInfo.setCity(dto.city());
+        userInfo.setAge(dto.age());
+        userInfo.setGender(dto.gender());
+        user.setUserInfo(userInfo);
         return user;
     }
 
--- src/main/java/kata/academy/socialnetwork/model/dto/response/user/UserResponseDto.java
+++ src/main/java/kata/academy/socialnetwork/model/dto/response/user/UserResponseDto.java
@@ -1,8 +1,13 @@
 package kata.academy.socialnetwork.model.dto.response.user;
 
+import kata.academy.socialnetwork.model.enums.Gender;
+
 public record UserResponseDto(
         Long id,
         String email,
         String firstName,
-        String lastName) {
+        String lastName,
+        String city,
+        Integer age,
+        Gender gender) {
 }
