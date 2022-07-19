6601c10f7d09e5be166ef5f58f3fa6220de049ba
Gunirs <contact@gunirs.me>
2022-07-19 10:16:42 +1000

    BACK13 Добавляет новые эндпоинты в UserPostRestController

--- src/main/java/kata/academy/socialnetwork/model/converter/PostMapper.java
+++ src/main/java/kata/academy/socialnetwork/model/converter/PostMapper.java
@@ -1,6 +1,7 @@
 package kata.academy.socialnetwork.model.converter;
 
 import kata.academy.socialnetwork.model.dto.request.post.PostPersistRequestDto;
+import kata.academy.socialnetwork.model.dto.request.post.PostUpdateRequestDto;
 import kata.academy.socialnetwork.model.dto.response.post.PostResponseDto;
 import kata.academy.socialnetwork.model.entity.Post;
 import kata.academy.socialnetwork.model.entity.User;
@@ -23,6 +24,14 @@ public final class PostMapper {
         post.setText(dto.text());
         post.setTags(dto.tags());
         post.setUser(user);
+        return post;
+    }
+
+    public static Post postUpdate(Post post, PostUpdateRequestDto dto) {
+        post.setTitle(dto.title());
+        post.setText(dto.text());
+        post.setTags(dto.tags());
+
         return post;
     }
 }
--- src/main/java/kata/academy/socialnetwork/web/rest/user/UserPostRestController.java
+++ src/main/java/kata/academy/socialnetwork/web/rest/user/UserPostRestController.java
@@ -7,17 +7,22 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import kata.academy.socialnetwork.model.api.Response;
 import kata.academy.socialnetwork.model.converter.PostMapper;
 import kata.academy.socialnetwork.model.dto.request.post.PostPersistRequestDto;
+import kata.academy.socialnetwork.model.dto.request.post.PostUpdateRequestDto;
 import kata.academy.socialnetwork.model.dto.response.post.PostResponseDto;
 import kata.academy.socialnetwork.model.entity.Post;
 import kata.academy.socialnetwork.model.entity.User;
 import kata.academy.socialnetwork.service.abst.entity.PostService;
+import kata.academy.socialnetwork.web.util.ApiValidationUtil;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -54,7 +59,49 @@ public class UserPostRestController {
         return Response.ok(PostMapper.toDto(postService.save(PostMapper.toEntity(dto, user))));
     }
 
-    //TODO добавить эндпоинт для получения по id GET/{postId} PostResponseDto
-    //TODO добавить эндпоинт для удаления (только свой пост) по id DELETE/{postId} Void
-    //TODO добавить эндпоинт для обновления (только свой пост) по id PUT/ PostResponseDto
+    @Operation(summary = "Эндпоинт для получения поста по его ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Пост успешно получен"),
+            @ApiResponse(responseCode = "400", description = "Пост не найден или клиент допустил ошибки в запросе")
+    })
+    @GetMapping("/{postId}")
+    public Response<PostResponseDto> getPostById(@PathVariable Long postId) {
+        Post post = postService.findById(postId).orElse(null);
+        ApiValidationUtil.requireNotNull(post, "Post not found");
+
+        return Response.ok(PostMapper.toDto(post));
+    }
+
+    @Operation(summary = "Эндпоинт для удаления своего поста по ID")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Пост успешно удален"),
+            @ApiResponse(responseCode = "400", description = "Клиент допустил ошибки в запросе")
+    })
+    @DeleteMapping("/{postId}")
+    public Response<Void> removePostById(@PathVariable Long postId) {
+        ApiValidationUtil.requireTrue(postService.existsById(postId), "Post not found");
+
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Post post = postService.findById(postId).orElseThrow();
+
+        ApiValidationUtil.requireTrue(post.getUser().equals(user), "You are not the author of this post");
+        postService.delete(post);
+        return Response.ok();
+    }
+
+    @Operation(summary = "Эндпоинт для редактирования своего поста")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Пост успешно изменен"),
+            @ApiResponse(responseCode = "400", description = "Клиент допустил ошибки в запросе")
+    })
+    @PutMapping
+    public Response<PostResponseDto> updatePost(@RequestBody @Valid PostUpdateRequestDto dto) {
+        ApiValidationUtil.requireTrue(postService.existsById(dto.id()), "Post not found");
+
+        User user = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        Post post = postService.findById(dto.id()).orElseThrow();
+
+        ApiValidationUtil.requireTrue(post.getUser().equals(user), "You are not the author of this post");
+        return Response.ok(PostMapper.toDto(postService.update(PostMapper.postUpdate(post, dto))));
+    }
 }
